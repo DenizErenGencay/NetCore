10a4577c333ec713d7e5391139b33ca83cc93820
DenizErenGencay <denizeren.gencay@vestel.com.tr>
2024-01-11 13:36:02 +0300

    Image upload actions implemented.

---
+++ WebApplication1/Controllers/ImagesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models.Domain;
+using WebApplication1.Models.DTO;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto requestDto)
+        {
+            ValidateFileUpload(requestDto);
+            if(ModelState.IsValid)
+            {
+                var imageDomainModel = new Image
+                {
+                    File = requestDto.File,
+                    FileExtension = Path.GetExtension(requestDto.File.FileName),
+                    FileSizeInBytes = requestDto.FileName.Length,
+                    FileName = requestDto.FileName,
+                    FileDescription = requestDto.FileDescription,
+                };
+
+                await imageRepository.Upload(imageDomainModel);
+
+                return Ok(imageDomainModel);
+            }
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto requestDto)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+            if (!allowedExtensions.Contains(Path.GetExtension(requestDto.File.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsopported file extension");
+            }
+
+            if(requestDto.File.Length > 10485760) 
+            {
+                ModelState.AddModelError("file", "Please upload a file smaller then 10mb");
+            }
+        }
+    }
+}
--- WebApplication1/Data/NZWalksDbContext.cs
+++ WebApplication1/Data/NZWalksDbContext.cs
@@ -16,6 +16,8 @@ namespace WebApplication1.Data
 
         public DbSet<Walk> Walks { get; set; }
 
+        public DbSet<Image> Images { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
